a68c2589113a977b0e29e132c655068424a7ecac
Nisal <nsltharaka@gmail.com>
2023-06-07 20:39:36 +0530

    UI designed for PQ, PO with controllers

---
+++ src/main/java/com/controllers/PurchaseOrderController.java
@@ -0,0 +1,20 @@
+package com.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class PurchaseOrderController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
